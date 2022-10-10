e894c8bf2a6d8f8fdc7051bec55eb0296a030c0e
gabrielbmmaia <gbmmaia.contato@gmail.com>
2022-10-10 20:03:13 -0300

    Adicionado recyclerview no layout da main activity e criação do layout dos itens do recyclerview

--- app/src/main/java/com/example/myrecipe/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/myrecipe/ui/activity/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import com.example.myrecipe.R
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/myrecipe/ui/model/Recipe.kt
+++ app/src/main/java/com/example/myrecipe/ui/model/Recipe.kt
@@ -1,5 +1,3 @@
 package com.example.myrecipe.ui.model
 
-class Recipe {
-
-}
+class Recipe ()
