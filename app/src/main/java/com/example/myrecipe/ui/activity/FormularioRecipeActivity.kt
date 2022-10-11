43da8bb56547da23bc453e0b229f7f6efda3e3ac
gabrielbmmaia <gbmmaia.contato@gmail.com>
2022-10-11 14:33:39 -0300

    Adicionado activity de Formulario e feature de adicionar novas receitas ao banco de dados

---
+++ app/src/main/java/com/example/myrecipe/ui/activity/FormularioRecipeActivity.kt
@@ -0,0 +1,54 @@
+package com.example.myrecipe.ui.activity
+
+import android.os.Bundle
+import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.myrecipe.R
+import com.example.myrecipe.databinding.AcitivityFormularioRecipeBinding
+import com.example.myrecipe.ui.database.AppDatabase
+import com.example.myrecipe.ui.model.Recipe
+import kotlinx.coroutines.launch
+
+
+class FormularioRecipeActivity : AppCompatActivity(R.layout.acitivity_formulario_recipe) {
+
+    private val recipeId = 0L
+    private val url: String? = null
+    private val binding by lazy {
+        AcitivityFormularioRecipeBinding.inflate(layoutInflater)
+    }
+    private val recipeDao by lazy {
+        AppDatabase.instance(this).recipeDao()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        configurarBotaoSalvar()
+        title = "Nova Receita"
+    }
+
+    private fun configurarBotaoSalvar() {
+        binding.formularioRecipeBotaoSalvar.setOnClickListener {
+            lifecycleScope.launch {
+                recipeDao.addRecipe(novaReceita())
+                finish()
+            }
+        }
+    }
+
+    private fun novaReceita(): Recipe {
+        val campoTitulo = binding.formularioRecipeEditTextTitulo.text.toString()
+        val campoIngredientes = binding.formularioRecipeEditTextIngredientes.text.toString()
+        val campoPreparo = binding.formularioRecipeEditTextPreparo.text.toString()
+
+        return Recipe(
+            id = recipeId,
+            titulo = campoTitulo,
+            ingrediente = campoIngredientes,
+            preparo = campoPreparo,
+            imagemUrl = url
+        )
+    }
+}
--- app/src/main/java/com/example/myrecipe/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/myrecipe/ui/activity/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.example.myrecipe.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.myrecipe.R
 import com.example.myrecipe.databinding.ActivityMainBinding
 import com.example.myrecipe.ui.database.AppDatabase
 import com.example.myrecipe.ui.recyclerview.adapter.MainActivityAdapter
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -24,8 +23,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        configuraRecyclerView()
+        configuracaoRecyclerView()
         atualizaTela()
+        configuracaoEfab()
     }
 
     private fun atualizaTela() {
@@ -36,7 +36,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun configuraRecyclerView() {
+    private fun configuracaoEfab(){
+        binding.activityMainEfab.setOnClickListener {
+            Intent(this, FormularioRecipeActivity::class.java)
+                .apply {
+                    startActivity(this)
+                }
+        }
+    }
+
+    fun configuracaoRecyclerView() {
         val recyclerView = binding.activityMainRecyclerView
         recyclerView.layoutManager = GridLayoutManager(this, 2)
         recyclerView.adapter = this.adapter
